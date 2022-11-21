15ea2b2ede2b95f8f9840860e48236f83e04bdd3
o4kas12 <ochkasdima92@yandex.ru>
2022-11-21 12:21:44 +0300

    add database

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from flask import Flask
 from flask import render_template
+import sqlite3 as sl
+
+con = sl.connect('database.db')
+
+# подготавливаем  запрос
+sql = 'INSERT INTO orders (product, amount, client_id) values(?, ?, ?)'
+# указываем данные для запроса
+data = [
+    ('табурет', 2, 1)
+]
+# добавляем запись в таблицу
+with con:
+    con.executemany(sql, data)
+
+# выводим содержимое таблицы с покупками на экран
+with con:
+    data = con.execute("SELECT * FROM orders")
+    for row in data:
+        print(row)
+
 
 app = Flask(__name__)
 
